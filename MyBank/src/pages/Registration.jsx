6e156e365d63ead55e2e2637baa96b1e85282125
shanishaju <shanishaju84210@gmail.com>
2025-02-25 13:54:04 +0530

    RHF is implemented on registration form

--- MyBank/src/pages/Registration.jsx
+++ MyBank/src/pages/Registration.jsx
@@ -1,81 +1,32 @@
-import { Button, TextField } from '@mui/material';
+import { Button, TextField, Typography } from '@mui/material';
 import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
-import FormControl from '@mui/material/FormControl';
-import FormLabel from '@mui/material/FormLabel';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import React, { useState } from 'react';
+import React from 'react';
 import dayjs from "dayjs";
+import { useForm, Controller } from 'react-hook-form';
 import { registerApi } from '../services/allApi';
 
 function Registration() {
-    // Create state for user details
-    const [userDetails, setUserDetails] = useState({
-        fname: '',
-        lname: '',
-        gender: "male",
-        dateOfBirth: '',
-        phonenum: ''
-    });
-    // console.log(userDetails);
+    const { register, handleSubmit, control, formState: { errors, isSubmitting } } = useForm();
 
+    const onSubmit = async (data) => {
+        console.log(data);
 
-    //validation
-    const [error, setError] = useState({});
-
-    const validate = () => {
-        let temerror = {};
-        if (!userDetails.fname) temerror.fname = "First Name is required";
-        if (!userDetails.lname) temerror.lname = "Last Name is required";
-        if (!userDetails.dateOfBirth) temerror.dateOfBirth = "Date of Birth is required";
-        if (!userDetails.phonenum) temerror.phonenum = "Phone Number is required";
-        else if (!/^\d{10}$/.test(userDetails.phonenum)) temerror.phonenum = "Enter a valid 10-digit phone number";
-
-
-        setError(temerror);
-        return Object.keys(temerror).length === 0;
-
-    }
-
-
-
-    const handleRegister = async () => {
-        //  const {fname,lname,dateOfBirth,phonenum} = userDetails //destructuring
-
-        //  if(!fname || !lname || !dateOfBirth || !phonenum){
-        //     alert("Please fill the fields completely");
-        //  }
-        //  else{
-        //     const response =await registerApi(userDetails);
-        //     console.log(response);
-        //     }
-        if (!validate()) return;
-        const result = await registerApi(userDetails);
-        // console.log(result.data.message);
-        if (result.status == 200) {
-            alert(result.data.message);
-
-            //clear datas
-            setUserDetails({
-                fname: '',
-                lname: '',
-                gender: "male",
-                dateOfBirth: '',
-                phonenum: ''
-
-            })
-
-
-
+        try {
+            const result = await registerApi(data);
+            if (result.status === 200) {
+                alert(result.data.message);
+            } else {
+                alert(`Error: ${result.response.data.message}`);
+            }
+        } catch (error) {
+            alert("An error occurred while submitting.");
         }
-        else {
-            alert(`Error message: ${result.response.data.message}`)
-
-        }
-    }
+    };
 
     return (
         <div className="container mt-5">
@@ -83,8 +34,7 @@ function Registration() {
                 Registration Form
             </h1>
             <div className="row maindiv2">
-
-                <div className='col-md-2'></div>
+                <div className="col-md-2"></div>
                 <div className="col-md-8">
                     <img
                         src="https://img.freepik.com/free-vector/global-stock-market-concept-illustration_114360-19030.jpg?t=st=1738483788~exp=1738487388~hmac=6c6f2a614cc4d4e13f3295c0fc20c42ac2f0294d4a88fde5071752092625783e&w=740"
@@ -95,91 +45,96 @@ function Registration() {
 
                 <div className="col-md-4" style={{ width: "100%" }}>
                     <div className="bg-light p-4 rounded" style={{ width: "100%" }}>
-                        <form className="mt-3">
-                            <div className="mb-3" style={{ display: "flex", gap: "10px", marginBottom: "10px" }}>
-                                <TextField
-                                    id="first-name"
-                                    label="First Name"
-                                    name="firstName"
-                                    variant="outlined"
-                                    value={userDetails.fname}
-                                    fullWidth
-                                    onChange={(e) =>
-                                        setUserDetails({ ...userDetails, fname: e.target.value })}
-                                    error={!!error.fname}
-                                    helperText={error.fname}
-                                />
-                                <TextField
-                                    id="last-name"
-                                    label="Last Name"
-                                    name="lastName"
-                                    variant="outlined"
-                                    value={userDetails.lname}
-                                    fullWidth onChange={(e) =>
-                                        setUserDetails({ ...userDetails, lname: e.target.value })}
-                                    error={!!error.lname}
-                                    helperText={error.lname}
-                                />
+                        <form className="mt-3" onSubmit={handleSubmit(onSubmit)}>
+                            <div className="mb-3" style={{ display: "flex", gap: "10px" ,marginBottom: "10px"}}>
+                                <div style={{ flex: 1 }}>
+                                    <TextField
+                                        label="First Name"
+                                        variant="outlined"
+                                        fullWidth
+                                        {...register('fname', { required: "First Name is required" })}
+                                        error={!!errors.fname}
+                                        helperText={errors.fname?.message}
+                                    />
+                                </div>
+                                <div style={{ flex: 1 }}>
+                                    <TextField
+                                        label="Last Name"
+                                        variant="outlined"
+                                        fullWidth
+                                        {...register('lname', { required: "Last Name is required" })}
+                                        error={!!errors.lname}
+                                        helperText={errors.lname?.message}
+                                    />
+                                </div>
                             </div>
 
-                            <div className="mb-3" style={{ marginBottom: "10px", display: "flex" }}>
+                            <div className="mb-3" style={{ display: "flex",marginBottom: "10px" }}>
                                 <div style={{ marginRight: "50px" }}>
                                     <LocalizationProvider dateAdapter={AdapterDayjs}>
-                                        <DatePicker
-                                            label="DoB"
-                                            value={userDetails.dateOfBirth ? dayjs(userDetails.dateOfBirth) : null}
-                                            onChange={(newValue) =>
-                                                setUserDetails({
-                                                    ...userDetails,
-                                                    dateOfBirth: newValue ? newValue.format("YYYY-MM-DD") : "",
-                                                })
-                                            }
-                                            error={!!error.dateOfBirth} helperText={error.dateOfBirth}
+                                        <Controller
+                                            name="dateOfBirth"
+                                            control={control}
+                                            rules={{
+                                                required: "Date of Birth is required",
+                                                // validate: value => {
+                                                //   const dateOfBirth = dayjs().diff(dayjs(value), 'year');
+                                                //   return dateOfBirth >= 18 || "You must be 18+ to register";
+                                                // }
+                                            }}
+                                            render={({ field }) => (
+                                                <DatePicker
+                                                    label="DoB"
+                                                    value={field.value ? dayjs(field.value) : null}
+                                                    onChange={(newValue) => field.onChange(newValue ? newValue.format("YYYY-MM-DD") : "")}
+                                                    slotProps={{ textField: { error: !!errors.dateOfBirth, helperText: errors.dateOfBirth?.message } }}
+                                                />
+                                            )}
                                         />
                                     </LocalizationProvider>
-                                    <p style={{ display: "none" }}>Selected Date: {userDetails.dateOfBirth || "None"}</p>
                                 </div>
-
                                 <div>
-                                    <RadioGroup
-                                        row
+                                    <Controller
                                         name="gender"
-                                        value={userDetails.gender}
-                                        onChange={(e) => setUserDetails({ ...userDetails, gender: e.target.value })}
-                                    >
-                                        <FormControlLabel value="male" control={<Radio />} label="Male" />
-                                        <FormControlLabel value="female" control={<Radio />} label="Female" />
-                                    </RadioGroup>
+                                        control={control}
+                                        rules={{ required: "Gender is required" }}
+                                        render={({ field }) => (
+                                            <RadioGroup row {...field}>
+                                                <FormControlLabel value="male" control={<Radio />} label="Male" />
+                                                <FormControlLabel value="female" control={<Radio />} label="Female" />
+                                            </RadioGroup>
+                                        )}
+                                    />
                                 </div>
                             </div>
 
                             <div className="mb-3" style={{ marginBottom: "10px" }}>
                                 <TextField
-                                    id="phone"
                                     label="Phone Number"
-                                    name="phone"
                                     variant="outlined"
-                                    value={userDetails.phonenum}
                                     fullWidth
-                                    onChange={(e) =>
-                                        setUserDetails({ ...userDetails, phonenum: e.target.value })}
-                                    error={!!error.phonenum}
-                                    helperText={error.phonenum}
+                                    {...register('phonenum', {
+                                        required: "Phone Number is required",
+                                        pattern: { value: /^\d{10}$/, message: "Enter a valid 10-digit phone number" }
+                                    })}
+                                    error={!!errors.phonenum}
+                                    helperText={errors.phonenum?.message}
                                 />
                             </div>
-
                             <div style={{ marginBottom: "10px", display: "flex", justifyContent: "space-between", gap: "10px" }}>
                                 <Button variant="contained" color="info" style={{ flex: 1, height: "50px" }}>
                                     CANCEL
                                 </Button>
-                                <Button variant="contained" color="success" style={{ flex: 1, height: "50px" }} onClick={handleRegister}>
-                                    SAVE
+                                <Button type="submit" style={{ flex: 1, height: "50px" }} variant="contained" color="success" disabled={isSubmitting}>
+                                    {isSubmitting ? 'Loading...' : 'Submit'}
                                 </Button>
                             </div>
+
+
                         </form>
                     </div>
                 </div>
-                <div className='col-md-2'></div>
+                <div className="col-md-2"></div>
             </div>
         </div>
     );
